7886362f90c192b81ae8f41123afeec3726d66a0
saimamilla <75039894+saimamilla@users.noreply.github.com>
2020-12-01 13:02:01 -0500

    Update Top100reviewers.java

--- Top100reviewers.java
+++ Top100reviewers.java
@@ -152,7 +152,7 @@ public class AmazonTopReviewers extends Configured implements Tool {
 				JsonElement jsonTree = parser.parse(jsonString);
 				
 				JsonObject jsonObject = jsonTree.getAsJsonObject();
-				if(jsonObject.get("verified").getAsString()==true){
+				if(jsonObject.get("verified").getAsBoolean()==true){
 				    String reviewerID = jsonObject.get("reviewerID").getAsString();
 				    String reviewer = jsonObject.get("reviewerName").getAsString();
 				    double overall = Double.parseDouble(jsonObject.get("overall").getAsString());
